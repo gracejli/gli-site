8943568612412a7d63401cb0f32ffa231dcde6d9
Grace <gracejieyi@gmail.com>
2026-02-12 19:36:28 -0800

    font and styling

--- app/page.tsx
+++ app/page.tsx
@@ -29,16 +29,16 @@ export default function App() {
             <p className="text-lg font-bianzhidai mb-2">
               grace li
             </p>
-            <p className="text-sm font-bold font-doto mb-2">
+            <p className="text-sm font-bold font-fe mb-2">
               tinkerer in los angeles, from a small town in michigan.
             </p>
-            <p className="text-sm font-bold font-doto mb-2">
+            <p className="text-sm font-bold font-fe mb-2">
               currently: i'm a technical artist at riot games working on making the game teamfight tactics as delightul as possible for players
             </p>
-            <p className="text-sm font-bold font-doto mb-2">
+            <p className="text-sm font-bold font-fe mb-2">
               previously: at microsoft and at columbia records 
             </p>
-            <p className ="text-sm font-bold font-doto mt-4 text-amber-300">
+            <p className ="text-sm font-bold font-febold mt-4 ">
               welcome to my internet room
             </p>
           </div>
@@ -46,7 +46,7 @@ export default function App() {
           {/* Carousel Section */}
           <div className="flex flex-col items-center">
              <Carousel images={myImages} />
-             <p className="text-sm font-doto mt-2">
+             <p className="text-sm font-fe mt-2">
                my bedroom over the years 
              </p>
           </div>
@@ -56,9 +56,9 @@ export default function App() {
         {/* RIGHT COLUMN: Links (40%) */}
         <div className="w-full md:w-[40%] flex flex-col h-full">
           <div className="h-full p-8 border-2 border-dashed rounded-xl transition-all duration-300 hover:border-amber-400 hover:bg-amber-400/5">
-            <h2 className="text-lg font-bold font-bianzhidai mb-4 text-amber-300">links</h2>
+            <h2 className="text-lg font-bold font-bianzhidai mb-4">links</h2>
             {/* Placeholder for your links */}
-            <ul className="space-y-2 font-doto text-sm font-bold">
+            <ul className="space-y-2 font-fe text-sm font-bold">
               <li><a href="https://www.are.na/gli/index" className="underline underline-offset-4 transition-all duration-200 hover:text-white hover:drop-shadow-[0_0_6px_rgba(253,224,71,0.8)">are.na</a></li>
               <li><a href="https://www.instagram.com/gracejli/" className="underline underline-offset-4 transition-all duration-200 hover:text-white hover:drop-shadow-[0_0_6px_rgba(253,224,71,0.8)">some of my favorite photos</a></li>
               <li><a href="https://www.yourworldoftext.com/%7Egracejli/" className="underline underline-offset-4 transition-all duration-200 hover:text-white hover:drop-shadow-[0_0_6px_rgba(253,224,71,0.8)">guestbook</a></li>
